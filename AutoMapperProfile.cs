db2dbe763c70cd182141dc8be72a70f8eb991f72
Tony Nguyen <57897153+tony-sn@users.noreply.github.com>
2025-06-15 13:04:13 +1000

    chore: install entityframework and dbContext

--- AutoMapperProfile.cs
+++ AutoMapperProfile.cs
@@ -7,7 +7,6 @@ public class AutoMapperProfile: Profile
         CreateMap<Character, GetCharacterDto>();
         CreateMap<AddCharacterDto, Character>();
         CreateMap<UpdateCharacterDto, Character>();
-        
     }
     
 }
