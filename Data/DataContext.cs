9d50cd65fce7f1d0b9f49df27384a4b247a7245b
Tony Nguyen <57897153+tony-sn@users.noreply.github.com>
2025-06-22 22:31:49 +1000

    One-To-One Relationship with Weapons

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -8,4 +8,5 @@ public class DataContext : DbContext
 
     public DbSet<Character> Characters { get; set; }
     public DbSet<User> Users => Set<User>();
+    public DbSet<Weapon> Weapons => Set<Weapon>();
 }
